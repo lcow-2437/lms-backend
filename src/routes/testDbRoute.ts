771f55c53de22913f24290bf1d44e607e32ffcf2
Palak <palakv213@gmail.com>
2025-04-11 15:47:23 -0400

    dont touch again testing / route

--- src/routes/testDbRoute.ts
+++ src/routes/testDbRoute.ts
@@ -3,7 +3,7 @@ import sequelize from '../config/database';
 
 const router = Router();
 
-router.get('/test-db', async (req: Request, res: Response) => {
+router.get('/', async (req: Request, res: Response) => {
   try {
     const [results] = await sequelize.query("SELECT NOW() AS time");
     // TypeScript workaround for unknown result type
