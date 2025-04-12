01a277097f4b7d57074e75632ab824b417d2556d
Palak <palakv213@gmail.com>
2025-04-12 13:57:02 -0400

    dont touch again testing / route

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -72,7 +72,7 @@ export class UserController {
   async getAllUsers(req: Request, res: Response): Promise<void> {
     try {
       const users = await this.userService.getAllUsers();
-      console.log("akgefks USERS");
+      console.log(users);
       res.json(users.map(user => this.toUserResponse(user)));
     } catch (error) {
       console.error(error);
